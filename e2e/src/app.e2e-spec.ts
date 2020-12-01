d873a87b9cb79a88bde3296835143bf82bc6b9f0
Jorge Turbica <723883@unizar.es>
2020-12-01 01:21:51 +0100

    Se han añadido nuevas acciones a los test e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import {browser, by, element, logging} from 'protractor';
+import {$, browser, By, by, element, ExpectedConditions, logging, protractor} from 'protractor';
 
 describe('Protractor Test Front-End', () => {
   let page: AppPage;
@@ -26,6 +26,34 @@ describe('Protractor Test Front-End', () => {
     element(by.id('pass-input')).sendKeys('12345678');
     element(by.id('access-button')).click();
     expect(pointer.getText()).toEqual('Mi Perfil');
+    expect(element(by.id('test-pointer-2')).getText()).toEqual('Usuarios Complementarios');
+  });
+
+  it('Save without changes', function() {
+    element(by.id('save-changes')).click();
+    browser.sleep(1000);
+    browser.refresh();
+    browser.sleep(1000);
+  });
+
+  it('Add interest', function() {
+    browser.driver.findElement(By.id('interest-option')).click(); // Opens the dropdown
+    browser.driver.findElement(By.xpath('//mat-option/span[contains(.,\'Java\')]')).click(); // Clicks on 'Java' option
+    element(by.id('add-interest-button')).click();
+    element(by.id('save-changes')).click();
+    browser.sleep(2000);
+    browser.refresh();
+    browser.sleep(2000);
+  });
+
+  it('Add knowledge', function() {
+    browser.driver.findElement(By.id('knowledge-option')).click(); // Opens the dropdown
+    browser.driver.findElement(By.xpath('//mat-option/span[contains(.,\'Angular\')]')).click(); // Clicks on 'Angular' option
+    element(by.id('add-knowledge-button')).click();
+    element(by.id('save-changes')).click();
+    browser.sleep(2000);
+    browser.refresh();
+    browser.sleep(5000);
   });
 
   afterEach(async () => {
