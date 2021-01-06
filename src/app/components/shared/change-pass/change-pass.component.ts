8109407107a803d9834d0e0d8ab18756386a6eb1
ferbercedo <fernando.alagon@gmail.com>
2021-01-06 19:11:36 +0100

    import md5 in change pass

--- src/app/components/shared/change-pass/change-pass.component.ts
+++ src/app/components/shared/change-pass/change-pass.component.ts
@@ -3,6 +3,8 @@ import { FormControl } from '@angular/forms';
 import { LoginService } from '../../../services/login.service';
 import { EditProfileService } from '../../../services/edit-profile.service';
 import { user } from 'src/app/models/user';
+import * as MD5 from 'md5';
+
 
 
 @Component({
